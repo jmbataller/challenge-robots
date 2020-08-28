2d0947aa02fbcea2b1173856d4c93fa087e1e16e
Jose Miguel Bataller <jose@pragmati.co>
2020-08-28 15:48:29 +0100

    spring-boot & gradle project

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # challenge-robots
-challenge-robots
+Martian Robots
+
+---
+
+Build
+
+> ./gradlew build
+
+
+
+---
+
+
+Requirements:
+
+- Uses spring-bool plugin that requires Gradle 6 (6.3 or later)
---
+++ src/test/java/org/test/robots/ChallengeRobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.robots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeRobotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
